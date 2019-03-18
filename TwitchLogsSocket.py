4a6e7b59903bd1f0fdc4d81a31bec4dfe835788a
SamuelHovind <42947898+SamuelHovind@users.noreply.github.com>
2019-03-18 16:44:17 +0100

    Update TwitchLogsSocket.py

--- TwitchLogsSocket.py
+++ TwitchLogsSocket.py
@@ -12,6 +12,11 @@ s.connect((connect_host,connect_port))
 s.send(("PASS {}\r\n").format(OAUTH_TOKEN).encode('utf-8'))
 s.send(("NICK {}\r\n").format(BOT_USERNAME).encode('utf-8'))
 s.send(('JOIN #{}\r\n').format(CHANNEL_NAME).encode('utf-8'))
+if os.path.isdir(os.getcwd()+'\\'+CHANNEL_NAME)==False:
+    os.mkdir(CHANNEL_NAME)
+    os.chdir(os.getcwd()+'\\'+CHANNEL_NAME)
+else:
+    os.chdir(os.getcwd()+'\\'+CHANNEL_NAME)
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s -- %(message)s',datefmt='%Y-%m-%d_%H:%M:%S',handlers=[logging.FileHandler('chat.log', encoding='utf-8')])
 
 while True:
@@ -22,4 +27,4 @@ while True:
         username = re.search("\w+", response).group(0)
         content = re.compile("^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
         message = content.sub("", response).rstrip('\n')
-        logging.info(username.title()+': '+message)
+        logging.info(message)
